e7a92fc4b9ba08dd448efc7c31f14e25cde94648
nisa <nisaagustiina@gmail.com>
2025-12-27 21:15:05 -0500

    Convert submodules to normal folders

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <footer>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default footer;
